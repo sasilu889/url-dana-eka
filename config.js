4fa252301fec942d6b7529e985c332851e9ee447
sasilu889 <bpremadasab@gmail.com>
2025-06-25 22:03:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || 'Manu-MD&bY1R2TRD#p2ZPacTKjeqR1d9wRT5sQxKKn7q7xM4urdPWCtrsfSE', // Put your session id here ( after linked check your number and get session id it need start with Manu-MD& ) 🤍
 PORT: process.env.PORT || '3000',
-MONGODB: process.env.MONGODB || 'mongodb+srv://nebocov664:IJKTsamFw4tU4rZJ@sasiludb.5ba1bev.mongodb.net/' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
+MONGODB: process.env.MONGODB || '//nebocov664:IJKTsamFw4tU4rZJ@sasiludb.5ba1bev.mongodb.net/' // අනිවාරෙන් Mongodb Url එකක් ඕනි දන්නෙ නැත්නම් මගෙ වීඩියෝ එක බලලා හදාගන්න.🔥
 };
